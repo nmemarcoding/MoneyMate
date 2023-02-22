f6784c9526af739dbf7ba8d936fa8b8f13b77807
nmemarcoding <nmemarcoding@gmail.com>
2023-02-22 11:34:36 -0800

    adding auto payment to expenses list if there nothing added

--- api/routes/budget.js
+++ api/routes/budget.js
@@ -3,6 +3,7 @@ const { model } = require("mongoose");
 const auth = require("../middlewear/auth");
 const Budget = require("../models/budget");
 const Income = require("../models/income");
+const Expense = require("../models/Expense");
 
 // rout to get budget base on user id an populate income and expense
 router.get("/", auth, async (req, res) => {
@@ -21,14 +22,44 @@ router.get("/", auth, async (req, res) => {
 // also cheack and if for any income for this mounth has isMontlyInome = true if is not creat new income with amunt of montly income saved in budget schema and also make isMontlyInome = tru and description = montly income
 router.get("/totalBudget", auth, async (req, res) => {
     try {
-        const budget = await Budget.find({ userId: req.userId }).populate("incomes").populate("expenses");
+        const budget = await Budget.find({ userId: req.userId }).populate("incomes").populate("expenses").populate("autoExpenses");
         let totalBudget = budget[0].totalBudget();
+        
         // check if in incomes array for existing month there is montly income
         const isMontlyIncome = budget[0].incomes.find((income) => {
          
             
             return income.isMontlyIncome=== true && income.date.getMonth() === new Date().getMonth() && income.date.getFullYear() === new Date().getFullYear()
         });
+        
+        // cheack if in expenses array for existing month and year there is auto expense
+        const isAutoExpense = budget[0].expenses.find((expense) => {
+            return expense.isAutoExpense === true && expense.date.getMonth() === new Date().getMonth() && expense.date.getFullYear() === new Date().getFullYear()
+        });
+        
+        //looping through expenses array and creat new expenses for each auto expense with sane amount and description
+        if (!isAutoExpense ) { 
+            budget[0].autoExpenses.forEach(async (expense) => {
+                    const newExpense = await Expense.create({
+                        userId: req.userId,
+                        amount: expense.amount,
+                        description: expense.description,
+                        date: new Date(),
+                        isAutoExpense: true,
+                    });
+                    // push new expense to expense array
+                    budget[0].expenses.push(newExpense);
+                    // save budget
+                    await budget[0].save();
+                    // add new expense amount to total budget
+                    totalBudget = totalBudget - expense.amount;
+                    console.log(totalBudget);
+                
+            });
+        }   
+        
+
+
         // if there is no montly income for this month creat new income with montly income amount and description montly income
         if (!isMontlyIncome && budget[0].montlyIncome !== 0) {
             const newIncome = await Income.create({
