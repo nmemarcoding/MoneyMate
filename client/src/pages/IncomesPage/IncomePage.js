c5ad0ef266efdf16287812a51b41343064df1f8d
nmemarcoding <nmemarcoding@gmail.com>
2023-03-01 10:50:43 -0800

    adding new income to the server

--- client/src/pages/IncomesPage/IncomePage.js
+++ client/src/pages/IncomesPage/IncomePage.js
@@ -1,10 +1,13 @@
 import { useState,useEffect } from 'react'
 import Navbar from '../../components/Navbar/Navber'
+import { publicRequest } from '../../hooks/requestMethods'
 
 export default function IncomePage() {
 
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")))
     const [income, setIncome] = useState([])
     const [totalIncomes, setTotalIncomes] = useState(0)
+    const [newIncome, setNewIncome] = useState({description:"",amount:""})
 
     useEffect(() => {
         const data = JSON.parse(localStorage.getItem("budget"))
@@ -31,6 +34,32 @@ export default function IncomePage() {
         setTotalIncomes(totalIncomes)
     }, [income])
 
+    // on change input handler for new income
+    const onChangeHandler = (e) => {
+        setNewIncome({...newIncome,[e.target.id]:e.target.value})
+        console.log(newIncome)
+    }
+
+    // on submit handler for new income
+    const onSubmitHandler = (e) => {
+        e.preventDefault()
+        publicRequest(user.accessToken).post("income",newIncome)
+        .then((res) => {
+
+            // add new income to begining of income array
+            setIncome([res.data,...income])
+            
+            console.log(res.data)
+            window.alert("Income added successfully.")
+
+        })
+        .catch((err) => {
+            console.log(err.respons.data)
+        })
+    
+    }
+
+
     
 
 
@@ -47,9 +76,9 @@ export default function IncomePage() {
             </div>
 
             <form className="flex flex-col md:flex-row items-center justify-center w-full mt-8">
-                <input className="w-full md:w-1/2 h-10 border-2 border-gray-300 rounded-lg px-3 mb-2 md:mr-2 md:mb-0 placeholder-gray-400" type="text" placeholder="Description" />
-                <input className="w-full md:w-1/2 h-10 border-2 border-gray-300 rounded-lg px-3 mb-2 md:mr-2 md:mb-0 placeholder-gray-400" type="number" placeholder="Amount" />
-                <button className="w-full md:w-1/2 h-10 bg-blue-500 text-white font-bold rounded-lg border border-blue-500 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800" type="submit">Add</button>
+                <input className="w-full md:w-1/2 h-10 border-2 border-gray-300 rounded-lg px-3 mb-2 md:mr-2 md:mb-0 placeholder-gray-400" type="text" placeholder="Description" id ="description" onChange={onChangeHandler}/>
+                <input className="w-full md:w-1/2 h-10 border-2 border-gray-300 rounded-lg px-3 mb-2 md:mr-2 md:mb-0 placeholder-gray-400" type="number" placeholder="Amount" id="amount" onChange={onChangeHandler}/>
+                <button className="w-full md:w-1/2 h-10 bg-blue-500 text-white font-bold rounded-lg border border-blue-500 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800" type="submit" onClick={onSubmitHandler}>Add</button>
             </form>
 
             <div className="flex items-center justify-center w-full mt-8">
